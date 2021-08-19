a4fb72abc725997599651768638a23f81c6ad6d1
Romo43 <lopezromo.fer43@gmail.com>
2021-08-18 22:22:33 -0500

    Summary

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # notes-app-v2
+https://youtu.be/zWax5QCWCXM?t=3565
---
+++ src/public/sockets.js
@@ -0,0 +1,9 @@
+const socket = io()
+
+const saveNote = (title, description) => {
+    socket.emit('client:newnote', {
+        title,
+        description,
+    });
+};
+socket.on('server:newnote', appendNote);
